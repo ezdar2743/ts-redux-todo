f1db473443045021d7ba93129c5d7fe9a652fb0a
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-29 12:04:01 +0900

    set up styledComponent and globalStyle

--- ts-redux-todo/src/index.tsx
+++ ts-redux-todo/src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { Globalstyle } from "./theme/global";
+import { theme } from "./theme/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Globalstyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ ts-redux-todo/src/theme/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#141414",
+  offText: "#d1ccc0",
+  onText: "#f5f6fa",
+  lineText: "#00a8ff",
+};
